e193a0f65a0fb900666d2096130488e94aff3969
ericwagnergithub <ericcwag@gmail.com>
2024-05-11 11:22:04 -0400

    Day 001

---
+++ Daily_Files/Day-001.py
@@ -0,0 +1,33 @@
+#The Start
+print("Hello World")
+
+#Multiple lines
+print("Step 1.")
+print("Step 2.")
+
+#Force Double Quotes in string
+print("She said 'hi' to him")
+print('She said "hi" to him')
+print("She said \"hi\" to him")
+
+#New Line
+print("hello world1. \nHello world2")
+
+#Combine Strings
+print("Hello" + " World")
+
+#Input Function
+input("What is your name?")
+
+#Single line for input & output (Order of operations)
+print("Hello " + input("What is your name?"))
+
+#Using Inputs As Variables
+num1 = int(input())
+num2 = int(input())
+print(num1 * num2)
+
+#len function
+numofletters = len("Eric")
+print(numofletters)
+
